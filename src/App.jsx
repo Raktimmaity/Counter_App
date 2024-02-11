e8b8185b85c227a0029c1480cf3969376b54f334
Raktimmaity <raktimmaity2003@gmail.com>
2024-02-11 13:26:46 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,17 @@ function App() {
   const removeValue = ()=>{
     setCount(count - 1);
   }
+  if(count == -1){
+    setCount(0);
+    alert("Counter value can not show below 0")
+  }
   return (
     <>
       <h1>Counter App</h1>
       <div className='main'>
-        <button onClick={addValue}> + </button>
-        <h2>The Value is: {count}</h2>
         <button onClick={removeValue}> - </button>
+        <h2>The Value is: {count}</h2>
+        <button onClick={addValue}> + </button>
       </div>
     </>
   )
